1a6c797daac94b878160fb9b7d4bd8b69f82579d
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-07-23 06:13:18 +0700

    dataframe for the positions

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,3 +1,5 @@
+from re import L
+from turtle import position
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
@@ -7,8 +9,9 @@ import pandas as pd
 # Create your views here.
 
 def home_view(request):
-    form = SalesSearchForm(request.POST or None)
     sales_df = None
+    positions_df = None
+    form = SalesSearchForm(request.POST or None)
     
     if request.method == 'POST':
         date_from = request.POST.get('date_from')
@@ -16,12 +19,30 @@ def home_view(request):
         chart_type = request.POST.get('chart_type')
         print(date_from, date_to, chart_type)
         
-        qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
-        if len(qs) > 0:
+        sale_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+        if len(sale_qs) > 0:
             
-            sales_df = pd.DataFrame(qs.values())
-            
+            sales_df = pd.DataFrame(sale_qs.values())
+            sales_df = pd.DataFrame(sale_qs.values())
+            positions_data = []
+            for sale in sale_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        'position_id': pos.id,
+                        'product' :  pos.product.name,
+                        'quantity': pos.quantity,
+                        'price': pos.price,
+                        
+                    }
+                    positions_data.append(obj)
+        
+            positions_df = pd.DataFrame(positions_data)
+            print('positions df')
+            print(positions_df)
+        
+        
             sales_df = sales_df.to_html()
+            positions_df = positions_df.to_html()
             print(sales_df)
         else:
             print('no data')
@@ -30,6 +51,7 @@ def home_view(request):
         
         'form': form,
         'sales_df':sales_df,
+        'positions_df': positions_df,
     }
     return render(request, 'sales/home.html', context)
 
