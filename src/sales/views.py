73ac2ad8b43c0af4726f248741e98bd4c5a57a97
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-07-22 19:13:13 +0700

    first querysets and dataframes

--- src/sales/views.py
+++ src/sales/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
+import pandas as pd
 
 # Create your views here.
 
@@ -13,6 +14,17 @@ def home_view(request):
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
         print(date_from, date_to, chart_type)
+        
+        qs = Sale.objects.filter(created__date=date_from)
+        obj = Sale.objects.get(id=1)
+       
+        print("##########")
+        df1 = pd.DataFrame(qs.values())
+        print(df1)
+        print("##########")
+        df2 = pd.DataFrame(qs.values_list())
+        print(df2)
+            
     context = {
         
         'form': form,
