f50d43515690b491d998d41abcb76a58c8f1b928
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-07-23 07:20:06 +0700

    the apply function

--- src/sales/views.py
+++ src/sales/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
 import pandas as pd
+from .utils import get_customer_from_id, get_salesman_from_id
 
 # Create your views here.
 
@@ -17,13 +18,16 @@ def home_view(request):
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
-        print(date_from, date_to, chart_type)
+        # print(date_from, date_to, chart_type)
         
         sale_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
         if len(sale_qs) > 0:
             
             sales_df = pd.DataFrame(sale_qs.values())
-            sales_df = pd.DataFrame(sale_qs.values())
+            sales_df['customer_id'] = sales_df['customer_id'].apply(get_customer_from_id)
+            sales_df['salesman_id'] = sales_df['salesman_id'].apply(get_salesman_from_id)
+            sales_df['created'] = sales_df['created'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            sales_df.rename ({'customer_id': 'customer', 'salesman_id': 'salesman'}, axis=1, inplace=True)
             positions_data = []
             for sale in sale_qs:
                 for pos in sale.get_positions():
@@ -38,8 +42,7 @@ def home_view(request):
                     positions_data.append(obj)
         
             positions_df = pd.DataFrame(positions_data)
-            print('positions df')
-            print(positions_df)
+            
         
         
             sales_df = sales_df.to_html()
