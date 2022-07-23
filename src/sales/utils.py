e193765ec5213d9e74237d055087d53298012752
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-07-23 18:31:47 +0700

    working on the charts part 2

--- src/sales/utils.py
+++ src/sales/utils.py
@@ -1,10 +1,10 @@
 from readline import parse_and_bind
-import uuid
+import uuid, base64
 from customers.models import Customer
 from profiles.models import Profile
 from io import BytesIO
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 
 
 def generate_code():
@@ -20,7 +20,7 @@ def get_customer_from_id(val):
     return customer
 
 def get_graph():
-    buffer = BytesIO
+    buffer = BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
     image_png = buffer.getvalue()
@@ -30,7 +30,23 @@ def get_graph():
     return graph
     
 
-def get_chart():
+def get_chart(chart_type, data, **kwargs):
+    plt.switch_backend('AGG')
+    fig = plt.figure(figsize=(10,4))
+    if chart_type == '#1':
+        print('bar chart')
+        # plt.bar(data['transaction_id'], data['price'])
+        sns.barplot(x='transaction_id', y='price', data=data)
+    elif chart_type == '#2':
+        print('pie chart')
+        labels = kwargs.get('labels')
+        plt.pie(data=data, x='price', labels=labels)
+    elif chart_type == '#3':
+        print('line chart')
+        plt.plot(data['transaction_id'], data['price'], color='green', marker='o')
+    else:
+        print('ups.... failed to indetify the chart type')
     
+    plt.tight_layout()
     chart = get_graph()
     return chart
--- src/sales/views.py
+++ src/sales/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
 import pandas as pd
-from .utils import get_customer_from_id, get_salesman_from_id
+from .utils import get_customer_from_id, get_salesman_from_id, get_chart
 
 # Create your views here.
 
@@ -14,6 +14,7 @@ def home_view(request):
     positions_df = None
     merged_df = None
     df = None
+    chart = None
     form = SalesSearchForm(request.POST or None)
     
     if request.method == 'POST':
@@ -51,13 +52,13 @@ def home_view(request):
 
             df = merged_df.groupby('transaction_id', as_index=False)['price'].agg('sum')
             
-            
-        
+            chart = get_chart(chart_type, df, labels=df['transaction_id'].values)
+            print('chart', chart)
             sales_df = sales_df.to_html()
             positions_df = positions_df.to_html()
             merged_df = merged_df.to_html()
             df = df.to_html()
-            print(sales_df)
+            
         else:
             print('no data')
             
@@ -68,6 +69,7 @@ def home_view(request):
         'positions_df': positions_df,
         'merged_df': merged_df,
         'df': df,
+        'chart': chart,
     }
     return render(request, 'sales/home.html', context)
 
