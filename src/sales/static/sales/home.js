1c1ff64147309cb519b656cbd1a4fcefe3818079
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-07-23 20:15:37 +0700

    adding the modal

--- src/sales/static/sales/home.js
+++ src/sales/static/sales/home.js
@@ -1,1 +1,11 @@
 console.log('hello world')
+
+const reportBtn = document.getElementById('report-btn')
+const img = document.getElementById('img')
+
+// console.log(reportBtn)
+// console.log(img)
+
+if (img){
+    reportBtn.classList.remove('invisible')
+}
